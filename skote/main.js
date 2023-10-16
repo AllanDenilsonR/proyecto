4b4f2edd64bad601b4e40b2bcc8fbf89c3c7e621
Allan Renderos <or16025@ues.edu.sv>
2023-10-16 17:39:40 -0600

    actulizado v16

--- skote/main.js
+++ skote/main.js
@@ -3244,7 +3244,7 @@ __webpack_require__.r(__webpack_exports__);
 // The list of file replacements can be found in `angular.json`.
 const environment = {
     production: false,
-    urlG: 'https://desplieugueallan2023.me'
+    urlG: 'https://desplieugueallan2023.me:8083'
 };
 
 
